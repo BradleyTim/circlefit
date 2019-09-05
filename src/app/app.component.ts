039710ec893f7cca3b98e89fedf832d9b6fc9592
BradleyTim <timbradley94@gmail.com>
2019-09-05 20:31:41 +0300

    update landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '🤸‍♂ CircleFit';
+
+  items = [
+    {
+      id: 1,
+      name: 'Tottenham',
+      size: 'Large',
+      img: 'https://images-na.ssl-images-amazon.com/images/I/71ISRedV-dL._UX522_.jpg'
+    },
+    {
+      id: 1,
+      name: 'Arsenal',
+      size: 'Medium',
+      img: 'https://images.sportsdirect.com/images/products/37708708_l.jpg'
+    },
+    {
+      id: 1,
+      name: 'Chelsea',
+      size: 'Small',
+      img: 'https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemShot/315x472/625408.jpg'
+    }
+  ];
 }
